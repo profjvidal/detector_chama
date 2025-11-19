cd491ccbab3fa5173c2bcc792e7b3df79304f408
Prof. JVidal <123177419+profjvidal@users.noreply.github.com>
2025-11-19 12:42:31 -0300

    Add files via upload

---
+++ codigo_detector_chama.ino
@@ -0,0 +1,19 @@
+int sensorChama = 2; //declaração da variável do sensor
+
+void setup() {
+  pinMode(sensorChama, INPUT); //Configuração do pino do sensor como entrada
+  Serial.begin(9600); // Configuração da comunicação serial
+}
+
+void loop() {
+  int leituraSensor = digitalRead(sensorChama);// Passando o valor obtido no sensor para a variável "leituraSensor"
+  // Se for detectado o espectro IR da chama imprime no serial "Pegou fogo!"
+  if (leituraSensor == HIGH) {
+    Serial.println("Pegou fogo!");
+  }
+  // Se NÃO for detectado o espectro IR da chama imprime no serial "Tudo certo."
+  else {
+    Serial.println("Tudo certo.");
+  }
+  delay(1000);//intervalo entre as leituras e respostas
+} // fim do código :)
